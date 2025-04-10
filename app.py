0e1cda34a4ab50471a1e89f16bf778177109e211
Shawaiz Sarfraz <shawaizsarfraz117@gmail.com>
2025-04-11 02:52:42 +0500

    update workflow file

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ def create_app():
 # For local development
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000)) 
     app.run(host='0.0.0.0', port=port, debug=False)
